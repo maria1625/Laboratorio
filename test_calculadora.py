25270a7deb28a597d28f40fd8104f47678229cc0
franklin cordoba <franklincordoba2001@gmail.com>
2026-09-19 17:28:00 -0500

    feat: agregar multiplicacion y division con sus pruebas

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,3 +5,9 @@ def test_sums_2_numbers():
 
 def test_restas_2_numbers():
     assert calculator().resta(8, 3) == 5
+
+def test_multiplicacion_2_numbers():
+    assert calculator().multiplicacion(2, 4) == 8
+
+def test_division_2_numbers():
+    assert calculator().division(10, 2) == 5
