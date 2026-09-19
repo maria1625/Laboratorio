9104bb39c8d57bbdec4c289d879797a526af86da
Maria Guerra <guerraruiz1625@gmail.com>
2026-09-19 16:29:17 -0500

    feat: afregar método resta() a calculator con su test

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,19 +1,7 @@
-import unittest
+from calculadora import calculator
 
-from calculadora import dividir, multiplicar, restar, sumar
+def test_sums_2_numbers():
+    assert calculator().suma(2, 3) == 5
 
-
-class CalculadoraTest(unittest.TestCase):
-    def test_operaciones_basicas(self):
-        self.assertEqual(sumar(2, 3), 5)
-        self.assertEqual(restar(8, 3), 5)
-        self.assertEqual(multiplicar(4, 3), 12)
-        self.assertEqual(dividir(10, 2), 5)
-
-    def test_division_entre_cero(self):
-        with self.assertRaises(ValueError):
-            dividir(10, 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_restas_2_numbers():
+    assert calculator().resta(8, 3) == 5
